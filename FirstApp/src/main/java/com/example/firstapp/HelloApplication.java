9f74cd41b6161a91b27ffed6c307b9df7504b233
Edgar Hernandez <ehernandez@griddynamics.com>
2024-11-21 16:46:56 -0600

    Use a button to disable another button

--- FirstApp/src/main/java/com/example/firstapp/HelloApplication.java
+++ FirstApp/src/main/java/com/example/firstapp/HelloApplication.java
@@ -12,17 +12,31 @@ import javafx.stage.Stage;
 public class HelloApplication extends Application {
 
     @Override
+    public void init() throws Exception {
+        System.out.println("Before the app starts!");
+        System.out.println(Thread.currentThread().getName());
+        Thread.sleep(5000);
+
+
+    }
+
+    @Override
     public void start(Stage stage) {
+        System.out.println(Thread.currentThread().getName());
         VBox root = new VBox();
         root.setAlignment(Pos.CENTER);
         root.setPadding(new Insets(25, 25, 25, 25));
         root.setSpacing(20);
         Label text = new Label("");
         Button button = new Button("Click me!");
+        Button button1 = new Button("Block!");
+        button1.setOnAction(actionEvent -> {
+            button.setDisable(!button.isDisable());
+        });
         button.setOnAction(event -> {
             text.setText("welcome to javafx application!");
         });
-        root.getChildren().addAll(text, button);
+        root.getChildren().addAll(text, button, button1);
         Scene scene = new Scene(root, 350, 300);
         stage.setScene(scene);
         stage.setTitle("First application");
